b74816f6aa592bf7ae9dfa0f263dee6f2c8d644d
marnelene <93662918+marnelene@users.noreply.github.com>
2021-11-23 10:20:08 +0100

    Rename 04_enrichment_tests_reactome_categories.R to 04_enrichm_tests.R

--- Placental_proteome/04_enrichm_tests.R
+++ Placental_proteome/04_enrichm_tests.R
@@ -1,6 +1,6 @@
 ###
 #
-# author: Maren-Helene Høie Degnes
+# author: Maren-Helene HÃ¸ie Degnes
 # date: 04.11.21
 # purpose: perform enrichment tests for the six classes of proteins:
 #          - proteins released from placenta to maternal circulation
